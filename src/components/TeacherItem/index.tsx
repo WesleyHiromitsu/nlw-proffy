f8042b81f3ebb04d9ac5bd01a89eb06e4ae5d071
WesleyHiromitsu <wes.hiro@gmail.com>
2020-08-04 15:36:45 -0300

    Added TeacherItem

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/14797491?s=460&u=d36d126cdc234188618c5d8e1c63e4448d0a7be7&v=4"
+          alt="Wesley Hiro"
+        />
+        <div>
+          <strong>Wesley Hiro</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+      <footer>
+        <p>
+          Preço/hora:
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
