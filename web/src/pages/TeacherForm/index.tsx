333c284edc41ec2153f8697df2adb7dafaa49afb
WesleyHiromitsu <wes.hiro@gmail.com>
2020-08-05 18:32:12 -0300

    Adjusting responsive teacherForm page

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 
+import warningIcon from "../../assets/images/icons/warning.svg";
+
 import "./styles.css";
 
 function TeacherForm() {
@@ -21,6 +23,22 @@ function TeacherForm() {
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="WhatsApp" />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula." />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
