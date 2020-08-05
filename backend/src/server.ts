662d052af2b8c02b193a95adfabe334ce8acd617
WesleyHiromitsu <wes.hiro@gmail.com>
2020-08-05 16:13:30 -0300

    Added cors on project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // para deixar as rotas em outro arquivo
 app.use(routes);
