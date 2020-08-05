e970d3dc9d0255c86031c34b643254e832ad510a
WesleyHiromitsu <wes.hiro@gmail.com>
2020-08-05 18:15:39 -0300

    Added Input component

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -9,6 +9,7 @@ import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -18,11 +19,12 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
         <Link to="/">
           <img src={backIcon} alt="Voltar" />
         </Link>
-        <img src={logoImg} alt="" />
+        <img src={logoImg} alt="Proffy" />
       </div>
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description ? <p>{props.description}</p> : null}
         {props.children}
       </div>
     </header>
